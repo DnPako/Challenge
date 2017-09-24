e6c6a52aff7bd678ccb0379e0255f8030195edac
Mouad MASTOUR <mouadmastour@gmail.com>
2017-09-24 15:26:19 +0100

    hide or display next button

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import _ from 'lodash';
 // Semantic UI
 import { Button } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
@@ -8,11 +9,18 @@ import 'semantic-ui-css/semantic.min.css';
 class NextButton extends React.Component {
     // Display next page of videos
     handleClick(){
-        const {data, page} = this.props;
+        const { data, page } = this.props;
         this.props.nextData(data, page);
     }
 
     render() {
+        const { data, page } = this.props;
+        const pages = _.size(data);
+        // Hide next button if no results are available
+        if(pages === 1 || pages === page)
+            return (
+                <div></div>
+            )
         return (
             <Button onClick={() => this.handleClick()} primary>Next</Button>
         )
